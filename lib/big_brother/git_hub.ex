dca827b9927cfabf64b6339837b091942e855709
Sean Heffernan <seanmheff@gmail.com>
2018-10-05 19:35:42 +0000

    Tidy up github module

--- lib/big_brother/git_hub.ex
+++ lib/big_brother/git_hub.ex
@@ -1,11 +1,10 @@
 defmodule BigBrother.GitHub do
+  alias HTTPotion.Response
+
   def fetch_version(repo, branch) do
     IO.puts "GitHub.fetch_version(#{repo}, #{branch})"
-
-    url = "https://api.github.com/repos/Jibestream/#{repo}/contents/build_tracker"
-    %HTTPotion.Response{ status_code: status_code, body: body } = fetch(url, branch)
-
-    case status_code do
+    %Response{ status_code: status, body: body } = fetch_file(repo, branch, "build_tracker")
+    case status do
       200 -> decode_api_response(body)
       _ -> "Unknown version"
     end
@@ -13,11 +12,8 @@ defmodule BigBrother.GitHub do
 
   def fetch_dependencies(repo, branch) do
     IO.puts "GitHub.fetch_dependencies(#{repo}, #{branch})"
-
-    url = "https://api.github.com/repos/Jibestream/#{repo}/contents/package.json"
-    %HTTPotion.Response{ status_code: status_code, body: body } = fetch(url, branch)
-
-    case status_code do
+    %Response{ status_code: status, body: body } = fetch_file(repo, branch, "package.json")
+    case status do
       200 -> body
       |> decode_api_response
       |> Poison.decode!
@@ -26,13 +22,13 @@ defmodule BigBrother.GitHub do
     end
   end
 
-  def fetch(url, branch) do
-    http_potion_config = [
+  def fetch_file(repo, branch, file) do
+    url = "https://api.github.com/repos/Jibestream/#{repo}/contents/#{file}"
+    HTTPotion.get(url, [
       basic_auth: { System.get_env("API_USER"), System.get_env("API_PASS") },
       headers: %{ "User-Agent": "Big-Brother" },
       query: %{ ref: branch }
-    ]
-    HTTPotion.get(url, http_potion_config)
+    ])
   end
 
   def decode_api_response(http_response) do
