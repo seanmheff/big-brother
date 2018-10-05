6f136e1952a86d3a31aa8cb00ace2708c40eb5d2
Sean Heffernan <seanmheff@gmail.com>
2018-10-05 19:52:59 +0000

    Refactor GitHub module

--- lib/big_brother/git_hub.ex
+++ lib/big_brother/git_hub.ex
@@ -3,19 +3,16 @@ defmodule BigBrother.GitHub do
 
   def fetch_version(repo, branch) do
     IO.puts "GitHub.fetch_version(#{repo}, #{branch})"
-    %Response{ status_code: status, body: body } = fetch_file(repo, branch, "build_tracker")
-    case status do
-      200 -> decode_api_response(body)
+    case fetch_file(repo, branch, "build_tracker") do
+      {:ok, file} -> file
       _ -> "Unknown version"
     end
   end
 
   def fetch_dependencies(repo, branch) do
     IO.puts "GitHub.fetch_dependencies(#{repo}, #{branch})"
-    %Response{ status_code: status, body: body } = fetch_file(repo, branch, "package.json")
-    case status do
-      200 -> body
-      |> decode_api_response
+    case fetch_file(repo, branch, "package.json") do
+      {:ok, file} -> file
       |> Poison.decode!
       |> Map.take(["version", "dependencies", "devDependencies"])
       _ -> %{}
@@ -24,14 +21,18 @@ defmodule BigBrother.GitHub do
 
   def fetch_file(repo, branch, file) do
     url = "https://api.github.com/repos/Jibestream/#{repo}/contents/#{file}"
-    HTTPotion.get(url, [
+    %Response{ status_code: status, body: body } = HTTPotion.get(url, [
       basic_auth: { System.get_env("API_USER"), System.get_env("API_PASS") },
       headers: %{ "User-Agent": "Big-Brother" },
       query: %{ ref: branch }
     ])
+    case status do
+      200 -> {:ok, decode_api_response(body)}
+      _ -> {:error}
+    end
   end
 
-  def decode_api_response(http_response) do
+  defp decode_api_response(http_response) do
     # Read about API response https://developer.github.com/v3/repos/contents/#get-contents
     http_response
     |> Poison.decode! # convert JSON to Elixir Map
