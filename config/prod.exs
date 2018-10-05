e9f85a44e51b1f39f7e80bd957e56e98b1df5ce5
Sean Heffernan <seanmheff@gmail.com>
2018-10-05 20:12:25 +0000

    Use debug level in prod

--- config/prod.exs
+++ config/prod.exs
@@ -18,8 +18,7 @@ config :big_brother, BigBrotherWeb.Endpoint,
   url: [host: "example.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
-# Do not print debug messages in production
-config :logger, level: :info
+config :logger, level: :debug
 
 # ## SSL Support
 #
